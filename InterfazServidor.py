7865e8042310620657e139365f68a9fb9da56965
MenaVhs <jgg1b@live.com.mx>
2022-04-17 14:18:33 -0500

    Update InterfazServidor.py

---
+++ InterfazServidor.py
@@ -0,0 +1,11 @@
+from asyncio.windows_events import NULL
+from tkinter import Tk, Label, Button
+import tkinter.font as tkFont
+import socket
+import sys
+import threading
+
+class InterfazGrafica:
+    nodo_servidor = 60000
+    
+    
